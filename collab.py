e3b7ec27a5eb35fab9bc4b9734a53a41a05364b9
Durgesh Singh <durgesh.singh90@gmail.com>
2024-09-17 18:37:02 +0100

    Update collab.py

--- collab.py
+++ collab.py
@@ -3,6 +3,11 @@
     {% for message in messages %}
         <div class="alert {% if message.tags %}alert-{{ message.tags }}{% endif %}">
             {{ message }}
+            {% if message.tags == 'success' and file_path %}
+            <br>
+            <span>File path: {{ file_path }}</span>
+            <button class="btn btn-link" onclick="openFileExplorer('{{ file_path }}')">Open</button>
+            {% endif %}
         </div>
     {% endfor %}
 </div>
