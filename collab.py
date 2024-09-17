fcfa11bb582a52f86bf99215cf1a21015a69741a
Durgesh Singh <durgesh.singh90@gmail.com>
2024-09-17 18:37:14 +0100

    Update collab.py

--- collab.py
+++ collab.py
@@ -12,3 +12,30 @@
     {% endfor %}
 </div>
 {% endif %}
+
+
+
+
+
+<script>
+function openFileExplorer(filePath) {
+    // Sending a POST request to a Django view to open the file explorer
+    fetch("{% url 'binblock:open_file_explorer' %}", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': '{{ csrf_token }}',
+        },
+        body: JSON.stringify({ 'file_path': filePath })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert('File explorer opened successfully.');
+        } else {
+            alert('Failed to open file explorer: ' + data.error);
+        }
+    });
+}
+</script>
+
