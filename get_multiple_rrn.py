8003b116d2979b3b96cdc4582422c386210ecf06
Durgesh Singh <durgesh.singh90@gmail.com>
2024-10-10 23:38:18 +0100

    Update get_multiple_rrn.py

--- get_multiple_rrn.py
+++ get_multiple_rrn.py
@@ -1,1 +1,96 @@
-31 31 3X 3X F2 3X X5 41 A8 EX 8X XX XX XX XX 1X 1X XX XX XX 31 34 33 36 32 35 39 36 3X 3X 35 3X 3X 31 3X 32 3X 3X 3X 3X 3X 3X 3X 3X 3X 3X 3X 3X 3X 3X 3X 31 33 3X 31 3X 31 3X 3X 3X 33 33 33 32 39 31 34 36 36 3X 32 34 31 3X 31 3X 3X 3X 33 33 33 32 39 3X 3X 35 39 3X 31 3X 3X 38 3X 3X 31 3X 3X 37 3X 31 31 31 31 3X 3X 3X 3X 3X 33 36 37 31 39 31 31 31 3X 3X 3X 3X 3X 33 36 31 39 3X 3X 33 37 33 36 32 35 39 36 3X 3X 35 3X 3X 31 3X 32 3D 32 34 3X 37 31 3X 31 31 36 3X 38 3X 3X 3X 37 39 39 3X 3X 3X 3X 3X 34 39 34 38 37 38 31 37 37 32 35 37 32 32 33 33 34 34 39 39 3X 3X 3X 31 3X 32 32 3X 3X 3X 3X 3X 36 36 36 35 37 41 5X 52 4D 41 3X 31 2X 2X 2X 2X 2X 2X 2X 45 4D 56 2X 4F 7X 74 69 6F 6E 61 5C 5C 6C 2X 35 35 2X 6F 6D 69 74 74 65 64 2X 5C 2X 2X 2X 2X 2X 2X 2X 2X 2X 2X 2X 2X 2X 38 32 36 38 32 36 38 32 36 31 31 3X 3X 3X 3X 3X 33 36 31 35 38 39
+<?xml version="1.0" encoding="utf-8"?>
+<EMVCoL3OnlineMessageFormat>
+  <LogDetails>
+    <Date-Time>2024-10-10T01:35:58.020Z</Date-Time>
+    <LoggingTool>
+      <ProductName>ASTREX</ProductName>
+      <ProductVersion>2487</ProductVersion>
+    </LoggingTool>
+    <SchemaSelectionIndex>1.0</SchemaSelectionIndex>
+    <L3OMLVersion>1.1</L3OMLVersion>
+    <Reference>Logged by Fime ASTREX on 2024-10-10T01:35:58.020Z</Reference>
+  </LogDetails>
+  <ConnectionList>
+    <Connection ID="{42CBCA90-18A7-4C3B-8BFA-24D3B1DE134E}">
+      <Protocol>
+        <FriendlyName>Visa BASE I</FriendlyName>
+        <SymbolicName>VISABASEI</SymbolicName>
+        <VersionInfo>2023.Q4</VersionInfo>
+      </Protocol>
+      <TCPIPParameters>
+        <Address/>
+        <Port>4323</Port>
+        <Header>4 bytes, PDP Order</Header>
+        <Client>false</Client>
+        <Format>EBCDIC</Format>
+      </TCPIPParameters>
+    </Connection>
+    <Connection ID="{CCE2A97D-B51C-4008-ACC4-5E971DEC382D}">
+      <Protocol>
+        <FriendlyName>Visa BASE I</FriendlyName>
+        <SymbolicName>VISABASEI</SymbolicName>
+        <VersionInfo>2023.Q4</VersionInfo>
+      </Protocol>
+      <TCPIPParameters>
+        <Address>unknown</Address>
+        <Port>4323</Port>
+        <Header>4 bytes, PDP Order</Header>
+        <Client>true</Client>
+        <Format>EBCDIC</Format>
+      </TCPIPParameters>
+    </Connection>
+    <Connection ID="{AA4D2A9C-F547-4720-8BD0-5D00CBCCC89A}">
+      <Protocol>
+        <FriendlyName>DCI Relay Xpress</FriendlyName>
+        <SymbolicName>DNI</SymbolicName>
+        <VersionInfo>2023.2</VersionInfo>
+      </Protocol>
+      <TCPIPParameters>
+        <Address/>
+        <Port>7003</Port>
+        <Header>4 bytes, Host Order</Header>
+        <Client>false</Client>
+        <Format>ASCII</Format>
+      </TCPIPParameters>
+    </Connection>
+    <Connection ID="{831543D0-69D6-48AA-81F7-34C091EB746A}">
+      <Protocol>
+        <FriendlyName>DCI Relay Xpress</FriendlyName>
+        <SymbolicName>DNI</SymbolicName>
+        <VersionInfo>2023.2</VersionInfo>
+      </Protocol>
+      <TCPIPParameters>
+        <Address>unknown</Address>
+        <Port>7003</Port>
+        <Header>4 bytes, Host Order</Header>
+        <Client>true</Client>
+        <Format>ASCII</Format>
+      </TCPIPParameters>
+    </Connection>
+  </ConnectionList>
+  <OnlineMessageList>
+ </OnlineMessageList>
+  <Signature xmlns="http://www.w3.org/2000/09/xmldsig#">
+    <SignedInfo>
+      <CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
+      <SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"/>
+      <Reference URI="">
+        <Transforms>
+          <Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/>
+        </Transforms>
+        <DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"/>
+        <DigestValue>eomumOjW4/M97X3BqLmn1ZfvuE8=</DigestValue>
+      </Reference>
+    </SignedInfo>
+    <SignatureValue>C364R7e56lWFFTZW519QsSAAZ1PREa46DBY3Bgw9MDybK2sQ/rtiFEtce3Vces6uB/jyHFOpnXt6BwofeqQDYAB/Bdm0dVX+V8ozKFoMnfmpCVtdPATc1BbrNegFL/Ib+LtdCH2TX5zOms8FFRqIaEnspQcsrbaBhL9Fc929szc=</SignatureValue>
+    <KeyInfo>
+      <KeyName>Fime ASTREX Key 1</KeyName>
+      <KeyValue>
+        <RSAKeyValue>
+          <Modulus>m81uEm3yhkWp+n+p0bB3ufwY/d4VjwpNXFvmkI6v6xStgmtjkWnBN7vXLwMKsg4ovxV2b4+hQ9dGDn+Yjk2Ftgp46cyYNC5MDSYJpibyiVplchFjZklHhvGBAZv3m9QjnyimMRFGJoztwWUkgKrlANklIfSISmAEdc0aB/tlQWM=</Modulus>
+          <Exponent>AQAB</Exponent>
+        </RSAKeyValue>
+      </KeyValue>
+    </KeyInfo>
+  </Signature>
+</EMVCoL3OnlineMessageFormat>
